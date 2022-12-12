03c670e3f40df52e2d57e4e4e1070efb9a4e30a2
Lyn <38800761+slynj@users.noreply.github.com>
2022-12-12 10:37:43 -0500

    Created Basic Textfield for Login
    - textfields for name, password, and email

--- app/src/main/java/com/havit/app/ui/login/LoginFragment.java
+++ app/src/main/java/com/havit/app/ui/login/LoginFragment.java
@@ -24,8 +24,8 @@ public class LoginFragment extends Fragment {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textNotifications;
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
