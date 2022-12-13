f82609fdd29a3690b89436adecf4d4dc86e23aae
John Seong <wonmor@gmail.com>
2022-12-13 11:10:02 -0500

    Updated the Camera API
    The previous API was deprecated, so I used the newer version of the Android Camera API as stated on the official documentation.

--- app/src/main/java/com/havit/app/ui/camera/CameraFragment.java
+++ app/src/main/java/com/havit/app/ui/camera/CameraFragment.java
@@ -1,28 +1,28 @@
 package com.havit.app.ui.camera;
 
-import static com.havit.app.ui.camera.CameraViewModel.getCameraInstance;
-
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 
-import com.havit.app.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
 import com.havit.app.databinding.FragmentCameraBinding;
 
+import java.util.concurrent.ExecutionException;
+
 public class CameraFragment extends Fragment {
-
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private FragmentCameraBinding binding;
-
-    private Camera mCamera;
-    private CameraPreview mPreview;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -32,15 +32,33 @@ public class CameraFragment extends Fragment {
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(getActivity(), mCamera);
-        FrameLayout preview = (FrameLayout) container.findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider, root);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(getContext()));
 
         return root;
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider, View root) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        // Unfinished code from here...
+        preview.setSurfaceProvider(root.findViewById().getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
 
     @Override
