f17833c25f4f00b1276598ac9437534caa9c5995
John Seong <wonmor@gmail.com>
2022-12-16 14:45:02 -0500

    Added Back button handler
    Added boilerplate code for handling back button in habit view

--- app/src/main/java/com/havit/app/ui/habit/HabitFragment.java
+++ app/src/main/java/com/havit/app/ui/habit/HabitFragment.java
@@ -5,11 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.havit.app.R;
 import com.havit.app.databinding.FragmentHabitBinding;
+
+import java.util.Objects;
 
 public class HabitFragment extends Fragment {
 
@@ -22,6 +28,16 @@ public class HabitFragment extends Fragment {
 
         binding = FragmentHabitBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // This callback will only be called when MyFragment is at least Started.
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Write logic here...
+            }
+        };
+
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
 
         return root;
     }
