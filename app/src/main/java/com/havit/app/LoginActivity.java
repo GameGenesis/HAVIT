a72c5f617a41d55ab8d3b0dd5f0fed2f0709693e
John Seong <wonmor@gmail.com>
2022-12-16 16:33:46 -0500

    Fixed the dark mode bug
    Fixed the issue where the password hide icon appeared to be black even in dark mode

--- app/src/main/java/com/havit/app/LoginActivity.java
+++ app/src/main/java/com/havit/app/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
