9dcb5361a618b94be5290c2c656910778e9665a1
John Seong <wonmor@gmail.com>
2022-12-19 10:30:33 -0500

    Update CameraFragment.java

--- app/src/main/java/com/havit/app/ui/camera/CameraFragment.java
+++ app/src/main/java/com/havit/app/ui/camera/CameraFragment.java
@@ -19,6 +19,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import com.havit.app.MainActivity;
 import com.havit.app.databinding.FragmentCameraBinding;
 
 import java.util.concurrent.ExecutionException;
