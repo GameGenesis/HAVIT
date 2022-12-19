b85e3f45854cc125dbc845bebbae7ae4044ee629
Lyn <38800761+slynj@users.noreply.github.com>
2022-12-19 09:40:00 -0500

    String value for the name, username, password
    - stores the input into a string variable
    - non mutable

--- app/src/main/java/com/havit/app/LoginActivity.java
+++ app/src/main/java/com/havit/app/LoginActivity.java
@@ -6,15 +6,26 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.havit.app.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private String[] userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        TextInputEditText nameTextField = binding.nameTextField;
+        TextInputEditText usernameTextField = binding.usernameTextField;
+        TextInputEditText passwordTextField = binding.passwordTextField;
+
+        String name = String.valueOf(usernameTextField.getText());
+        String username = String.valueOf(usernameTextField.getText());
+        String password = String.valueOf(usernameTextField.getText());
 
         Button button = binding.submitButton;
 
