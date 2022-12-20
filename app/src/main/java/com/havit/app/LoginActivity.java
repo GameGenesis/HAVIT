a588884f6934eda9957789ace04206975ca8e14a
Rayan Kaissi <gamegenesisdev@gmail.com>
2022-12-20 09:07:37 -0500

    Get text from name field
    * Log text from name field after submitting

--- app/src/main/java/com/havit/app/LoginActivity.java
+++ app/src/main/java/com/havit/app/LoginActivity.java
@@ -2,10 +2,13 @@ package com.havit.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.havit.app.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,6 +22,10 @@ public class LoginActivity extends AppCompatActivity {
         Button button = binding.submitButton;
 
         button.setOnClickListener(v -> {
+            TextInputLayout textInputLayout = findViewById(R.id.nameField);
+            EditText editText = textInputLayout.getEditText();
+            String text = editText.getText().toString();
+            Log.d("Name field", text);
             Intent i = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(i);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
