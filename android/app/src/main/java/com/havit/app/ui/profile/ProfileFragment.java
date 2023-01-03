acd03e416a8f9f1be9f45ce7a09dd167d9c49bf5
John Seong <wonmor@gmail.com>
2023-01-03 14:15:43 +0900

    Removed unnecessary imports

--- android/app/src/main/java/com/havit/app/ui/profile/ProfileFragment.java
+++ android/app/src/main/java/com/havit/app/ui/profile/ProfileFragment.java
@@ -2,29 +2,25 @@ package com.havit.app.ui.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.havit.app.LoginActivity;
 import com.havit.app.R;
 import com.havit.app.databinding.FragmentProfileBinding;
-
-import org.w3c.dom.Text;
 
 import java.util.Locale;
 import java.util.Objects;
