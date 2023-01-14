110701b1824e5ebbcf89b67b4b5f3a33d5f08c75
Lyn <38800761+slynj@users.noreply.github.com>
2023-01-14 04:42:05 -0500

    Added Javadocs for PopupDialog
    - Added Javadocs for PopupDialog

--- android/app/src/main/java/com/havit/app/ui/edit/PopupDialog.java
+++ android/app/src/main/java/com/havit/app/ui/edit/PopupDialog.java
@@ -12,6 +12,13 @@ import androidx.fragment.app.DialogFragment;
 import com.havit.app.databinding.PopupLayoutBinding;
 
 public class PopupDialog extends DialogFragment {
+    /**
+     * Inflates the layout for the PopupDialog and sets up the dismiss button to close the dialog when clicked
+     * @param inflater the layout inflater
+     * @param container the parent view group
+     * @param savedInstanceState the saved state of the fragment
+     * @return the inflated view
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         PopupLayoutBinding binding = PopupLayoutBinding.inflate(inflater, container, false);
