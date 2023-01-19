476a3ed0fe57519499e31bdb1874042a47dfedde
John Seong <wonmor@gmail.com>
2023-01-19 16:44:23 -0500

    Update video.py

--- server/video.py
+++ server/video.py
@@ -1,4 +1,3 @@
-# importing libraries
 import os
 import cv2 
 from PIL import Image 
@@ -7,11 +6,7 @@ from flask import current_app
 import firebase_admin
 from firebase_admin import firestore, storage, auth, credentials
 
-# Function to generate video
-# For the reference, check out:
-# https://www.geeksforgeeks.org/python-create-video-using-multiple-images-using-opencv/
-
-def export_video(firebase_token, timeline_name, template_name):
+def export_video(firebase_token, timeline_name, template_name, fps=30):
     # Set up the credentials as an environmental variable
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "assets/havitcentral-b63dac00aa76.json"
 
@@ -29,18 +24,29 @@ def export_video(firebase_token, timeline_name, template_name):
     bucket = storage.bucket('gs://havitcentral.appspot.com')
 
     # Get the images from the specified folder in the storage bucket
-    image_files = bucket.get_files({ prefix: 'users/' + user_email + '/' + timeline_name + '/' })
-    images = asyncio.gather(*(file.download() for file in image_files))
+    blobs = bucket.list_blobs(prefix='users/' + user_email + '/' + timeline_name + '/')
 
-    global mean_width, mean_height
+    image_files = []
+
+    for blob in blobs:
+        if blob.content_type.startswith('image'):
+            image_files.append(blob)
+
+    # Create a directory to store the images
+    if not os.path.exists(f'./temp/{user_email}/{timeline_name}/'):
+        os.makedirs(f'./temp/{user_email}/{timeline_name}/')
     
+    # Download the images to the local directory
+    for index, blob in enumerate(image_files):
+        blob.download_to_filename(f'./temp/{user_email}/{timeline_name}/IMG_{index}')
+
     mean_height = 0
     mean_width = 0
     
-    num_of_images = len(images)
+    num_of_images = len(image_files)
 
-    for image_data in images:
-        image = Image.open(BytesIO(image_data))
+    for image_file in image_files:
+        image = Image.open(BytesIO(image_file.download_as_string()))
         width, height = image.size
         mean_width += width
         mean_height += height
@@ -54,50 +60,29 @@ def export_video(firebase_token, timeline_name, template_name):
     mean_width = int(mean_width / num_of_images)
     mean_height = int(mean_height / num_of_images)
 
-    # Resizing of the images to give
-    # them same width and height 
-    for image_data in images:
-        file = BytesIO(image_data)
-
-        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"):
-            # opening image using PIL Image
-            im = Image.open(file)
+    # Resizing of the images to give them same width and height 
+    for index, image_file in enumerate(image_files):
+        im = Image.open(f'./temp/{user_email}/{timeline_name}/IMG_{index}')
     
-            # im.size includes the height and width of image
-            width, height = im.size   
-            print(width, height)
+        width, height = im.size   
     
-            # resizing 
-            imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS) 
+        # resizing 
+        imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS)
 
-            # Convert the modified image back to a BytesIO object
-            imResize.save( file, 'JPEG', quality = 95) # setting quality
+        # Saving the resized image
+        imResize.save(f'./temp/{user_email}/{timeline_name}/IMG_{index}', 'JPEG', quality = 95)
 
-            image_data = file.read()
+    video_name = f'./temp/{user_email}/{timeline_name}/export.avi'
 
-    # Calling the generate_video function
-    generate_video(images, user_email, timeline_name, template_name)
-
-# Video Generating function
-def generate_video(images, user_email, timeline_name, template_name, fps=30):
-    video_name = f'./temp/{user_email}/{timeline_name}.avi'
-
-    frame = cv2.imread(BytesIO(images[0]))
-  
-    # setting the frame width, height width
-    # the width, height of first image
-    height, width, layers = frame.shape  
-  
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video = cv2.VideoWriter(video_name, fourcc, float(fps), (width, height)) 
-
+    out = cv2.VideoWriter(video_name, fourcc, fps, (mean_width, mean_height))
     duration = get_data_from_firestore(template_name)
 
     previous_end_time = 0;
     
     # Appending the images to the video one by one
-    for i, image in enumerate(images): 
+    for i in range(num_of_images):
         # Duration is in milliseconds, so we need to convert it to seconds
         current_start_time = duration[i][0] / 1000
         current_end_time = duration[i][1] / 1000
@@ -112,46 +97,51 @@ def generate_video(images, user_email, timeline_name, template_name, fps=30):
             # Add a gap between the previous frame and the current frame
             for _ in range(int(fps * (current_start_time - previous_end_time))):
                 # Add a black frame
-                video.write(cv2.imread(BytesIO(black_image_object)))
+                out.write(cv2.imread(BytesIO(black_image_object)))
         
         for _ in range(int(fps * current_end_time - current_start_time)):
             # Repeat each frame for the specified duration
-            video.write(cv2.imread(BytesIO(image)))
-      
+            img_path = f'./temp/{user_email}/{timeline_name}/IMG_{index}'
+            frame = cv2.imread(img_path)
+
+            # Writing the extracted images
+            out.write(frame)
+
+
     # Deallocating memories taken for window creation
     cv2.destroyAllWindows() 
-    video.release()  # releasing the video generated
+    out.release()
 
-    current_app.logger.info("users/" + user_email + "/" + timeline_name + "/export.avi")
+    # Upload the video file to the storage bucket
+    video_blob = bucket.blob(f'users/{user_email}/{timeline_name}/export.avi')
+    video_blob.upload_from_filename(video_name)
 
-    # Create a reference to the video in Firebase Storage
-    video_blob = bucket.blob("users/" + user_email + "/" + timeline_name + "/export.avi")
+    # Delete the temp images
+    for index in range(num_of_images):
+        os.remove(f'./temp/{user_email}/{timeline_name}/IMG_{index}')
 
-    # Open the video file
-    with open(video_name, "rb") as video_file:
-        # Upload the video to Firebase Storage
-        video_blob.upload_from_file(video_file)
+    # delete temp folder
+    os.rmdir(f'./temp/{user_email}/{timeline_name}/')
 
-def get_data_from_firestore(template_name):
-    doc_ref = db.collection('templates').document(template_name)
 
-    doc = doc_ref.get()
+    def get_data_from_firestore(template_name):
+        doc_ref = db.collection('templates').document(template_name)
 
-    duration = []
+        doc = doc_ref.get()
 
-    if doc.exists:
-        # Iterate through the map field
-        for key, value in doc.to_dict().get("timestamp").items():
-            key_list = key.split("-")
+        duration = []
 
-            start_time_str = key_list[0]
-            start_millis = int(start_time_str[0:2]) * 60000 + int(start_time_str[2:4]) * 1000 + int(start_time_str[4:6])
-            
-            end_time_str = key_list[1]
-            end_millis = int(end_time_str[0:2]) * 60000 + int(end_time_str[2:4]) * 1000 + int(end_time_str[4:6])
+        if doc.exists:
+            # Iterate through the map field
+            for key, value in doc.to_dict().get("timestamp").items():
+                key_list = key.split("-")
 
-            duration.append([start_millis, end_millis])
+                start_time_str = key_list[0]
+                start_millis = int(start_time_str[0:2]) * 60000 + int(start_time_str[2:4]) * 1000 + int(start_time_str[4:6])
+                
+                end_time_str = key_list[1]
+                end_millis = int(end_time_str[0:2]) * 60000 + int(end_time_str[2:4]) * 1000 + int(end_time_str[4:6])
 
-    return duration
-            
-# TO DO: ADD MEMBERSHIP_ENABLED BOOLEAN TO FIRESTORE USER.JSON AS WELL AS FIRST_TIME_USER BOOLEAN
+                duration.append([start_millis, end_millis])
+
+        return duration
