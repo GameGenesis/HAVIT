87dd5215ec940394ef7eca8057c9163b4492a6c1
John Seong <wonmor@gmail.com>
2023-01-22 15:03:38 -0500

    Update Privacy.tsx

--- web/src/pages/Privacy.tsx
+++ web/src/pages/Privacy.tsx
@@ -16,7 +16,7 @@ export default function Privacy() {
                 as="big"
                 variant="h1"
                 className="text-3xl mr-4 cursor-pointer py-1.5 font-normal normal-case">
-                    This page is used to inform the users of the website John Seong regarding our policies regarding the collection, use, and disclosure of Personal Information if anyone decided to use our service.{" "}
+                    This page is used to inform the users of our product regarding our policies regarding the collection, use, and disclosure of Personal Information if anyone decided to use our service.{" "}
                     If you choose to use <span className="title">HAVIT.</span> then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.{" "}
                     The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible on our official website unless otherwise defined in this Privacy Policy.
             </Typography>
